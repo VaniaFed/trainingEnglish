891906978abe69d9a0a8b8898605a51d8f522742
Yang Vanya <vaniafed@WebMachine.WebServer>
2018-04-08 20:00:11 +0300

    some change

--- source/pages/index/js/index.js
+++ source/pages/index/js/index.js
@@ -1,16 +1,6 @@
-webpackJsonp([0],[
-/* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__normalize_css__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__normalize_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__normalize_css__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__index_scss__ = __webpack_require__(2);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__index_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__index_scss__);
-
-
-
+import "./normalize.css";
+import "./index.scss";
 'use strict';
 
 const pickRandWord = function(el) {
@@ -1831,19 +1821,3 @@ window.onload = function () {
 		});
 	}
 };
-
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-/* 2 */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ })
-],[0]);
